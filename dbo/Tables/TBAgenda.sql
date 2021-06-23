430b2380ca41fc46637cae50e3496b1961c3050e
emannuelvv <54732031+emannuelvv@users.noreply.github.com>
2021-06-23 20:24:25 -0300

    up banco

---
+++ dbo/Tables/TBAgenda.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBAgenda] (
+    [Id]              INT           IDENTITY (1, 1) NOT NULL,
+    [Titulo]          VARCHAR (200) NULL,
+    [DataDeCriacao]   DATETIME      NULL,
+    [DataDeConclusao] DATETIME      NULL,
+    [Percentual]      INT           NULL,
+    [Prioridade]      VARCHAR (20)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ dbo/Tables/TBContato.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBContato] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]     VARCHAR (200) NULL,
+    [Email]    VARCHAR (50)  NULL,
+    [Telefone] VARCHAR (15)  NULL,
+    [Empresa]  VARCHAR (50)  NULL,
+    [Cargo]    VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
